3f8ebbbf0e251d860ada9c39ce0b63f02f6e3560
Felipe A. Moreno <fmorenovr@gmail.com>
2018-03-24 17:59:18 -0500

    Update HC06.ino

--- HC06.ino
+++ HC06.ino
@@ -5,15 +5,22 @@ Connect
   RX <---- TX
 */
 // Variables
-int ArduLED = 13;				      //Internal Arduino´s LED
-char ssid[10] = "Jnzds";	    // Name for Bluetooth.
-char baudios   = '4';		      // 1=>1200 baudios, 2=>2400, 3=>4800, 4=>9600 (default), 5=>19200, 6=>38400, 7=>57600, 8=>115200
-char password[10]	= "1234";		// Password for pairing
+int ArduLED       = 13;        //Internal Arduino´s LED
+char ssid[10]     = "Jnzds";   // Name for Bluetooth.
+char baudios      = '4';       // 1=>1200
+                               // 2=>2400
+                               // 3=>4800
+                               // 4=>9600 (default)
+                               // 5=>19200
+                               // 6=>38400
+                               // 7=>57600
+                               // 8=>115200
+char password[10] = "1234";    // Password for pairing
 
 void setup(){
-  Serial.begin(9600);		// 9600bauds is the deafult baudrate for these modules.
-					              //if it´s not working try changing this baudrate to match your HC-06 initial setup
-	// Waiting time (10 seconds) onboard LED is ON:
+  Serial.begin(9600);  // 9600bauds is the deafult baudrate for these modules.
+  //if it´s not working try changing this baudrate to match your HC-06 initial setup
+  // Waiting time (10 seconds) onboard LED is ON:
   pinMode(ArduLED,OUTPUT);
   digitalWrite(ArduLED,HIGH);
   delay(10000);
@@ -33,7 +40,7 @@ void setup(){
 }
 
 void loop(){
-	// After programing bluetooth, onboard LED will Blink.
-	digitalWrite(ArduLED, !digitalRead(ArduLED));
-	delay(500);
+  // After programing bluetooth, onboard LED will Blink.
+  digitalWrite(ArduLED, !digitalRead(ArduLED));
+  delay(500);
 }
